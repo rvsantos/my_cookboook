9ead17a93c950cb68fc5700135ea9793834258d2
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-08-24 21:50:53 -0300

    User register recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,11 +6,29 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(set_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
   def show; end
 
   private
 
   def set_params_id
     @recipe = Recipe.find(params[:id])
+  end
+
+  def set_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+                                    :cook_time, :ingredients, :cook_method)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # get 'home/index'
   root to: 'recipes#index'
 
-  resources :recipes, only: %i[index show]
+  resources :recipes, only: %i[index new create show]
 end
