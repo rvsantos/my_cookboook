771006fc4630ce2b0052d70c689d686aaee9155f
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-08-25 01:51:07 -0300

    User login and logout

---
+++ spec/features/user_auth_site_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'User authenticates on site' do
+  scenario 'successfully' do
+    User.create(email: 'email@email.com', password: '123456')
+
+    visit root_path
+    click_on 'Entrar'
+    within '#new_user' do
+      fill_in "Email",	with: "email@email.com"
+      fill_in "Password",	with: "123456"
+      click_on 'Log in'
+    end
+
+    expect(page).to_not have_content('Entrar')
+    expect(page).to have_content('Sair')
+  end
+
+  scenario 'successfully' do
+    User.create(email: 'email@email.com', password: '123456')
+
+    visit root_path
+    click_on 'Entrar'
+    within '#new_user' do
+      fill_in "Email",	with: "email@email.com"
+      fill_in "Password",	with: "123456"
+      click_on 'Log in'
+    end
+    click_on 'Sair'
+
+    expect(page).to_not have_content('Sair')
+    expect(page).to have_content('Entrar')
+  end
+end
